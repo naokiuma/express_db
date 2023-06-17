3df3b24b1da473321bc2f0c1224fd7efb24d5f2f
nao mac <sagalumaata@gmail.com>
2023-06-18 01:28:45 +0900

    create作成

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express()
 const port = 3000;
 const mysql = require('mysql')
 
-
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: ''
 });
+
+
 
 con.connect(function(err) {
     if (err) throw err;
@@ -35,8 +36,56 @@ con.connect(function(err) {
     //     console.log('table created')
     // })
 
+
+	//selectクエリ
+	const sql = "select * from users"
+	con.query(sql,function(err,result,fields){
+		if(err) throw err;
+		console.log('リザルと')
+		console.log(result)
+
+	})
+
+
+
+	app.get('/',(req,res)=>{
+		const sql = 'select * from users';
+		con.query(sql,function (err,result,fields){
+			res.send(result)
+		})
+	})
+
+	app.get("/create/:name",(req,res) =>{
+		const sql = `INSERT users(name,email) VALUES('${req.params.name}','${req.params.name}@gmail.com')`;
+		
+		// con.query(sql,function (err,result,fields){
+		// 	res.send(result)
+		// 	console.log(result)
+		// })
+
+	})
+
+	app.get("/home/index", (req, res) => {
+		res.status(200).send("OK");
+	});
+
+	app.get("/home/create", (req, res) => {
+		res.status(200).send("OKda");
+	});
+
+	
+
     
 });
-
-app.get('/',(req,res) => res.send('hellow worlddaaadd!'))
 app.listen(port,() => console.log(`example on port ${port}`))
+
+// app.get('/',(req,res) => res.send('hellow worlddaaadd!'))
+
+
+
+// app.get('/',(req,res)=>{
+// 	const sql = 'select * from users';
+// 	con.query(sql,function (err,result,fields){
+// 		res.send(result)
+// 	})
+// })
